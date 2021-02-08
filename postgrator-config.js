0d452a58e74381fea87bd4638c87121c58f2ca9a
Liron de Castro <lirondecastro@Lirons-MacBook-Air.local>
2021-02-07 20:50:09 -0800

    ssl still

--- postgrator-config.js
+++ postgrator-config.js
@@ -6,5 +6,8 @@ module.exports = {
   "connectionString": (process.env.NODE_ENV === 'test')
     ? process.env.TEST_DATABASE_URL
     : process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    }
 }
 
