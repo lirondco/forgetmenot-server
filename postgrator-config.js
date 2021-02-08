74eae0e69dd455bf2ddecca053eb0d0e4ffaf452
Liron de Castro <lirondecastro@Lirons-MacBook-Air.local>
2021-02-07 18:21:12 -0800

    stillllllll trying to do migrations omg half a day

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,9 +3,9 @@ require('dotenv').config();
 module.exports = {
   "migrationsDirectory": "migrations",
   "driver": "pg",
-  "host": process.env.DB_HOST,
-  "port": process.env.DB_PORT,
-  "database": process.env.DB_NAME,
-  "username": process.env.DB_USER,
-  "password": process.env.DB_PASS
+  "ssl": !!process.env.SSL,
+  "connectionString": (process.env.NODE_ENV === 'test')
+    ? process.env.TEST_DATABASE_URL
+    : process.env.DATABASE_URL,
+    ssl: process.env.NODE_ENV === "production"
 }
