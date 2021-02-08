e61f116ad19ec4caf5b56f33a89b7219dc9ba48e
Liron de Castro <lirondecastro@Lirons-MacBook-Air.local>
2021-02-07 18:10:48 -0800

    aaaaaaaaah migrations

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,9 +3,9 @@ require('dotenv').config();
 module.exports = {
   "migrationsDirectory": "migrations",
   "driver": "pg",
-  "host": process.env.DATABASE_HOST,
-  "port": process.env.DATABASE_PORT,
-  "database": process.env.DATABASE_NAME,
-  "username": process.env.DATABASE_USER,
-  "password": process.env.DATABASE_PASS
+  "host": process.env.DB_HOST,
+  "port": process.env.DB_PORT,
+  "database": process.env.DB_NAME,
+  "username": process.env.DB_USER,
+  "password": process.env.DB_PASS
 }
