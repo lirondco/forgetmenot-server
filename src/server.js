bd2106553362818c685f0688799490214e968e35
Liron de Castro <lirondecastro@Lirons-MacBook-Air.local>
2021-02-07 21:25:36 -0800

    SSL

--- src/server.js
+++ src/server.js
@@ -1,9 +1,8 @@
-require('dotenv').config()
-
-const knex = require('knex')
 const app = require('./app')
 const { PORT, DATABASE_URL } = require('./config')
+const pg = require ('pg')
 
+pg.defaults.ssl = process.env.NODE_ENV === "production"
 const db = knex({
   client: 'pg',
   connection: DATABASE_URL,
