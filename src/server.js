06cc5da556fbaa5d1eeeb01d5486e6f9ac32f244
Liron de Castro <lirondecastro@Lirons-MacBook-Air.local>
2021-02-07 19:42:11 -0800

    still migrations

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
-require('dotenv').config()
-
 const knex = require('knex')
 const app = require('./app')
 const { PORT, DATABASE_URL } = require('./config')
+const pg = require ('pg')
 
+pg.defaults.ssl = process.env.NODE_ENV === "production"
 const db = knex({
   client: 'pg',
   connection: DATABASE_URL,
@@ -13,4 +13,3 @@ app.set('db', db)
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
-})
